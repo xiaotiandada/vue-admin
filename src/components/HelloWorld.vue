6b22c1abc7b9fab733fcba6af0fbd989b4c0e69f
xiaotiandada <952822399@qq.com>
2018-04-05 19:15:06 +0800

    eslint

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -86,9 +86,10 @@
 <script>
 export default {
   name: 'HelloWorld',
-  data () {
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      smsg: 'Welcome to Your Vue.js App'
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,15 +1,23 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import 'normalize.css' // A modern alternative to CSS resets
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 import router from './router'
-
+import locale from 'element-ui/lib/locale/lang/en'
 Vue.config.productionTip = false
+Vue.use(ElementUI, {
+  locale
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
